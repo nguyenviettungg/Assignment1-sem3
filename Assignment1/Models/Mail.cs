abd06cf0ed1116c67a05b99d3e75baa745088d19
nguyenviettungg <nguyentung696969@gmail.com>
2020-09-09 21:22:59 +0700

    nop bai

---
+++ Assignment1/Models/Mail.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1.Models
+{
+    class Mail
+    {
+        private string email;
+        private string title;
+        private string description;
+
+        public Mail(string email, string title, string description)
+        {
+            this.email = email;
+            this.title = title;
+            this.description = description;
+        }
+        public string Email
+        {
+            get => email;
+            set => email = value;
+        }
+        public string Title
+        {
+            get => title;
+            set => title = value;
+        }
+        public string Description
+        {
+            get => description;
+            set => description = value;
+        }
+
+        public string ToString()
+        {
+            return Email + "--" + Title + "--" + Description + "\n";
+        }
+    }
+
+}
